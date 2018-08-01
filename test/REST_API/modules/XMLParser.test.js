341139aeb5bfcb26416550ca769abb994b68b762
[Luis Flores] <[24.flores.luis@gmail.com]>
2018-08-01 16:16:53 -0500

    Updated Schedular.js from Chris' files

--- test/REST_API/modules/XMLParser.test.js
+++ test/REST_API/modules/XMLParser.test.js
@@ -3,11 +3,13 @@ var assert = require('chai');
 var XMLParser = require('../../../REST_API/modules/XMLParser');
 
 //Test Methods
-describe('XMLParser', function() {
-    describe('getData', function () {
-        it('should return list of currency pairs along with their rates',function () {
-            assert.typeOf(value, name, "[message]");
-        })
+// describe('XMLParser', function() {
+//     describe('getData', function () {
+//         it('should return list of currency pairs along with their rates',function () {
+//             assert.typeOf(value, name, "[message]");
+//         })
         
-    })
-});
+//     })
+// });
+
+XMLParser.getData()
