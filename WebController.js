3524bfe125d3ead14b1f2dd79d98e9b420a8259c
[Luis Flores] <[24.flores.luis@gmail.com]>
2018-08-01 16:23:56 -0500

    Chris Updates prior to notification implementation

--- WebController.js
+++ WebController.js
@@ -6,6 +6,10 @@ const expressHandlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const getSymbolPosition = require('./REST_API/REST_API').getSymbolPosition;
 const retrieveCurrentSymbolRate = require('./REST_API/REST_API').retrieveCurrentSymbolRate;
+const beginIntervalParsing = require('./modules/scheduler').beginIntervalParsing;
+
+const https = require('https');
+const fs = require('fs');
 
 //Required Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,50 +39,123 @@ app.listen(port, () => {
 });
 
 //--------------------------User Interaction Below-----------------------------------
-var blank = "N/A";
+const BLANK = "";
+const HIDE = "hidden";
+const SHOW = "";
+const NOERRORS = null;
+var intervalObjs = [];
 
 //GET index page
-app.get('/', (request, response) => {
-  response.render('index', {
-    textBoxSymbolDisplayValue: "",
-    textBoxRateDisplayRate: "",
-    displaySymbol: blank,
-    displayRate: blank,
-    displayTargetRate: blank,
-    displayTrackingRequestSubmitTime: blank
+app.get('/', (req, res) => {
+  console.log("61");
+  clearIntervalParsing();
+  console.log("62");
+  //renderIndexPage parameter descriptions:
+  //(req, res, validatedSymbol, targetRate, currentRate, rateSuccessTime, startCardViewValue, successNotifyViewValue, trackSubmitTime, errors, textBoxSymbolDisplayValue, textBoxRateDisplayRate)
+  renderIndexPage(req, res, BLANK, BLANK, BLANK, BLANK, HIDE, HIDE, BLANK, NOERRORS, BLANK, BLANK)
+  console.log("63");
+});
+
+//First Page's Submit Button is clicked to start tracking. NEED TO ADD clearing any previous tracking.
+app.post('/track', (req, res) => {
+  console.log("8");
+  clearIntervalParsing();
+  console.log("9");
+  checkUserInputForErrors(req, res, (validatedSymbol, targetRate, currentRate) => {
+    console.log("10");
+    //renderIndexPage parameter descriptions:
+    //(req, res, validatedSymbol, targetRate, currentRate, rateSuccessTime, startCardViewValue, successNotifyViewValue, trackSubmitTime, errors, textBoxSymbolDisplayValue, textBoxRateDisplayRate)
+    var trackSubmitTime = new Date().toLocaleTimeString();
+    console.log("11");
+    renderIndexPage(req, res, validatedSymbol, targetRate, currentRate, BLANK, SHOW, HIDE, trackSubmitTime, NOERRORS, BLANK, BLANK);
+    console.log("12");
+    startIndexPageIntervalParsing(validatedSymbol, targetRate, req, res);
+    console.log("13");
   });
 });
 
-//First Page's Submit Button is clicked
-app.post('/track', (req, res) => {
-  checkUserInputForErrors(req, res, function (validatedSymbol, targetRate, currentRate) {
-    res.render('index', {
-      textBoxSymbolDisplayValue: "",
-      textBoxRateDisplayRate: "",
-      displaySymbol: validatedSymbol,
-      displayRate: currentRate,
-      displayTargetRate: targetRate,
-      displayTrackingRequestSubmitTime: new Date().toLocaleTimeString()
-    });
+//new Date().toLocaleTimeString()
+
+//This just renders the page with the given parameters.
+function renderIndexPage(req, res, validatedSymbol, targetRate, currentRate, rateSuccessTime, startCardViewValue, successNotifyViewValue, trackSubmitTime, errors, textBoxSymbolDisplayValue, textBoxRateDisplayRate) {
+  console.log("6");
+  res.render('index', {
+    errors: errors,
+    textBoxSymbolDisplayValue: textBoxSymbolDisplayValue,
+    textBoxRateDisplayRate: textBoxRateDisplayRate,
+    displaySymbol: validatedSymbol,
+    displayRate: currentRate,
+    displayTargetRate: targetRate,
+    displayTrackingRequestSubmitTime: trackSubmitTime,
+    displayRateSuccessTime: rateSuccessTime,
+
+    //Hide or don't hide the startCardView and the successNotifyView
+    startCardView: startCardViewValue,
+    successNotifyView: successNotifyViewValue
   });
-});
+  console.log("7");
+}
+
+function startIndexPageIntervalParsing(validatedSymbol, targetRate, req, res) {
+  console.log("64");
+  beginIntervalParsing(validatedSymbol, (currentRate) => {
+    console.log("65");
+    //For testing, assume that we want the current rate to increase up to the target rate.
+    console.log(validatedSymbol + " is at " + currentRate);
+    if (parseFloat(currentRate) >= parseFloat(targetRate)) {
+      console.log("66");
+      var rateSuccessTime = new Date().toLocaleTimeString();
+      console.log(rateSuccessTime);
+      //renderIndexPage parameter descriptions:
+      //(req, res, validatedSymbol, targetRate, currentRate, rateSuccessTime, startCardViewValue, successNotifyViewValue, trackSubmitTime, errors, textBoxSymbolDisplayValue, textBoxRateDisplayRate)
+      console.log("4"); 
+      clearIntervalParsing();
+      console.log("67");
+      //renderIndexPage(req, res, validatedSymbol, targetRate, currentRate, rateSuccessTime, HIDE, SHOW, BLANK, NOERRORS, BLANK, BLANK);
+      console.log("Target rate reached. Stopping interval parsing.");
+ 
+      console.log("1");
+    }
+  },
+    (intervalObj) => {
+      console.log("2");
+      intervalObjs.push(intervalObj);
+      console.log("3");
+    }
+  );
+}
+
+function clearIntervalParsing() {
+  console.log("68");
+  intervalObjs.forEach((intervalObj) => {
+    console.log("Clear an intervalObj");
+    clearInterval(intervalObj);
+  });
+  console.log('Interval parsing cleared');
+}
 
 //Server side user input validation. Check to see if user's input is valid.
 //If it is not, then redisplay the page with error message(s).
 function checkUserInputForErrors(req, res, callback) {
+  console.log("14");
   getSymbolPosition(req.body.textBoxSymbol, (position) => {
+    console.log("15");
     let errors = [];
-
     //Check for valid currency symbol
     if (!req.body.textBoxSymbol) {
+      console.log("70");
       errors.push({ text: 'Please enter the currency symbol.' });
+      console.log("71");
     }
     else if (position == null) {
+      console.log("72");
       errors.push({ text: 'The entered currency symbol does not match any known currency symbol.' });
     }
     else {   //Currency symbol was valid. Retrieve the current rate.
+      console.log("16");
       retrieveCurrentSymbolRate(req.body.textBoxSymbol, function (fetchedRate) {
-        //Check for valid target rate
+        console.log("17");
+        //Check for valid target rate 
         if (!req.body.textBoxRate) {
           errors.push({ text: 'Please enter the target rate.' });
         }
@@ -91,26 +168,24 @@ function checkUserInputForErrors(req, res, callback) {
         else if (req.body.textBoxRate == fetchedRate) {
           errors.push({ text: 'This symbol is already at the target rate' });
         }
-
+        console.log("18");
         //Respond
         if (errors.length > 0) {
           //If any errors are encountered, then we will redisplay the webpage.
           //Whatever the user has entered into the input fields will stay.
           //The default display in the View the Tracked Symbol and Current Rate box will stay.
-          res.render('index', {
-            errors: errors,
-            textBoxSymbolDisplayValue: req.body.textBoxSymbol,
-            textBoxRateDisplayRate: req.body.textBoxRate,
-            displaySymbol: blank,
-            displayRate: blank,
-            displayTargetRate: blank,
-            displayTrackingRequestSubmitTime: blank
-          });
 
+          //renderIndexPage parameter descriptions:
+          //(req, res, validatedSymbol, targetRate, currentRate, rateSuccessTime, startCardViewValue, successNotifyViewValue, trackSubmitTime, errors, textBoxSymbolDisplayValue, textBoxRateDisplayRate)
+          console.log("19");
+          renderIndexPage(req, res, BLANK, BLANK, BLANK, BLANK, HIDE, HIDE, BLANK, errors, req.body.textBoxSymbol, req.body.textBoxRate);
+          console.log("20");
           //If we do not get any errors, then proceed with the callback function.
         } else {
-          //callback(symbol, desiredRate, currentRate, isTheDesiredRateGreateThanTheCurrentRate); 
+          console.log("21");
+          //callback(symbol, desiredRate, currentRate); 
           callback(req.body.textBoxSymbol, req.body.textBoxRate, fetchedRate);
+          console.log("22");
         }
       });
     }
@@ -118,15 +193,9 @@ function checkUserInputForErrors(req, res, callback) {
       //If any errors are encountered, then we will redisplay the webpage.
       //Whatever the user has entered into the input fields will stay.
       //The default display in the View the Tracked Symbol and Current Rate box will stay.
-      res.render('index', {
-        errors: errors,
-        textBoxSymbolDisplayValue: req.body.textBoxSymbol,
-        textBoxRateDisplayRate: req.body.textBoxRate,
-        displaySymbol: blank,
-        displayRate: blank,
-        displayTargetRate: blank,
-        displayTrackingRequestSubmitTime: blank
-      });
+      console.log("23");
+      renderIndexPage(req, res, BLANK, BLANK, BLANK, BLANK, HIDE, HIDE, BLANK, errors, req.body.textBoxSymbol, req.body.textBoxRate);
+      console.log("81");
     }
   });
 }
--- modules/scheduler.js
+++ modules/scheduler.js
@@ -4,20 +4,22 @@ const REST_API = require('../REST_API/REST_API');
 //Exports
 module.exports.beginIntervalParsing = beginIntervalParsing;
 
+//Module variables
+const delayBetweenParses = 1000;
+
 //Functions
-function beginIntervalParsing(timeDelay, symbolToParse, callback) {
+function beginIntervalParsing(symbolToParse, repeatedFunction, callback) {
+  console.log("24");
   const intervalObj =
     setInterval(() => {
+      console.log("25");
       REST_API.retrieveCurrentSymbolRate(symbolToParse, (currentRate) => {
-        console.log(currentRate);
+        console.log("26");
+        repeatedFunction(currentRate);
+        console.log("27");
       });
-    }, timeDelay);
+    }, delayBetweenParses);
+  console.log('About to do a callback');
   callback(intervalObj);
+  console.log("28");
 }
-
-//Testing
-beginIntervalParsing(1000, 'EURUSD', (intervalObj) => {
-  setTimeout(() => {
-    clearTimeout(intervalObj);
-  }, 10000);
-});
