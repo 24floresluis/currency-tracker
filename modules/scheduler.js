c3cddca94b2a28efd03801ec8e18a78f574ec8ce
[Luis Flores] <[24.flores.luis@gmail.com]>
2018-08-03 17:26:57 -0500

    Removed Comments

--- modules/scheduler.js
+++ modules/scheduler.js
@@ -9,17 +9,11 @@ const delayBetweenParses = 1000;
 
 //Functions
 function beginIntervalParsing(symbolToParse, repeatedFunction, callback) {
-  console.log("24");
   const intervalObj =
     setInterval(() => {
-      console.log("25");
       REST_API.retrieveCurrentSymbolRate(symbolToParse, (currentRate) => {
-        console.log("26");
         repeatedFunction(currentRate);
-        console.log("27");
       });
     }, delayBetweenParses);
-  console.log('About to do a callback');
   callback(intervalObj);
-  console.log("28");
 }
