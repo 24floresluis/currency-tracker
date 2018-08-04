17a99341c8ba8775aad5268a76abfeaa1f64d85e
[Luis Flores] <[24.flores.luis@gmail.com]>
2018-08-04 12:30:15 -0500

    Updated Modules for Testing

--- test/WebController.test.js
+++ test/WebController.test.js
@@ -1,9 +1,23 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
+const request = require('supertest');
+rewire = require('rewire');
+const app = require('../WebController');
 
-const webcontroller = require('../WebController');
+//Socket.io testing tools
+const io = require('socket.io-client');
+const socketUrl = 'http://localhost:8000';
+
+var options = {
+    transports: ['websocket'],
+    'force new connection' : true
+}
 
 describe('WebController', ()=> {
+
+    describe('Sockets', ()=> {
+        var client1;
+    })
     it('should successfuly return a webpage: status 200');
     it('should route correctly');
     it('should return land in 404 if route does not exist');
